12ea506c43e7dd337baf2fa3a3024f6aea1b5f20
LOMFM <brightdawn019@gmail.com>
2019-09-04 23:15:31 +0800

    DB Setting on server

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/exparel'
+    url: 'mongodb+srv://exparel:admin123@cluster0-lohrf.mongodb.net/test?retryWrites=true'
+    // url: 'mongodb://localhost:27017/exparel'
 }
